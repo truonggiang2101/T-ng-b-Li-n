c860c5263b88a6d3e0b4388baeed3bf94ffe9ab2
truonggiang2101 <166736986+truonggiang2101@users.noreply.github.com>
2024-04-11 22:48:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Hôm nay trời nhiều mây, anh cũng vậy, 
+    nhiều mây và nhiều nhớ em`,
+    btnIntro: 'hihi',
+    title: 'Bầu trời xanh, làn mây trắng.',
+    desc: 'Em yêu nắng hay yêu anh?',
+    btnYes: 'Tất nhiên là yêu anh rồi <3',
+    btnNo: 'Không, anh mơ à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì buộc yêu <333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'anh mua cho bé cái áo rồi á nha',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/Dang200306/' 
+}
